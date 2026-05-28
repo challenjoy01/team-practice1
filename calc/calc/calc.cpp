b0e9ea42402f39af3a5e0e701431209e4e66defb
challenjoy01 <challenjoy@naver.com>
2026-05-28 12:41:34 +0900

    줄바꿈, 직접 연산 기능 추가

--- calc/calc/calc.cpp
+++ calc/calc/calc.cpp
@@ -2,15 +2,33 @@
 #include "add.h"
 #include "sub.h"
 #include "mul.h"
+using namespace std;
 
 int main()
 {
-
+    int x = 0;
+    int y = 0;
+    char oper = 'a';
     int add_ = add(1,4);
     int sub_ = sub(4, 1);
     int mul_ = mul(2, 4);
-    printf("%d", add_);
-    printf("%d", sub_);
-    printf("%d", mul_);
+    printf("%d\n", add_);
+    printf("%d\n", sub_);
+    printf("%d\n", mul_);
+    printf("|숫자 연산자 숫자| 입력. 공백이 꼭 있어야 합니다.\n");
+    cin >> x >> oper >> y;
+
+    if (oper == '+')
+    {
+        printf("%d + %d = %d", x, y, add(x, y));
+    }
+    else if (oper == '-')
+    {
+        printf("%d - %d = %d", x, y, sub(x, y));
+    }
+    else if (oper == '*')
+    {
+        printf("%d * %d = %d", x, y, mul(x, y));
+    }
 
 }
